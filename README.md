99b9b4f2afe67d698268d4ee8868b7da2c3e7ccc
auridevil <onailerua@hotmail.com>
2016-12-13 12:56:10 +0100

    readme completed

--- README.md
+++ README.md
@@ -1,13 +1,90 @@
-azure-image-server
-==========
-*ready to deploy image (and file) server for azure webapps with azure storage*
+# azure-image-server
 
-simple use
-==========
+*ready to deploy image (and file) server for azure webapps with image scaling*
+
+## Use
+
 
 ```
 git clone https://github.com/auridevil/azure-image-server.git
-npm install
+source env.sh
 npm start
 ```
+
+## Configuration
+
+All the configurations are done using environment variables. 
+
+The connection string to the azure storage (mandatory):
+```
+AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=xxxx;AccountKey=yyyyyyyyyyy"
+
+```
+
+The secret key to decode jwt token (almost mandatory):
+```
+JWT_KEY="jwt-secret-key-here"
+
+```
+
+The time to wait before unlinking temp files:
+```
+UNLINK_TIMEOUT="2000"
+```
+
+Various:
+```
+PORT="8080"
+HOST="localhost"
+FILE_CONTAINER_NAME="files"
+IMG_CONTAINER_NAME="images"
+```
+
+## Deploy
+
+- create a azure web app
+- create a deploy slot from git for the web app and use the git address in the package.json (replace "tbi")
+- configure the web app with the env vars
+- npm run azure:register:xxx (where xxx is dev / test / prod)
+- npm run deploy:xxx (where xxx is dev / test / prod)
+
+
+## Notes 
+
+The repository is packed with runtimes AND node_modules, it should be used to deploy on azure because default node runtimes are 32 bit and some dependency doesnt' work on windows32.
+Please make sure that you don't have a global gitignore for .exe or use git add with -f option.
+
+## Requests
+
+- GET:    image/filename.ext : request a image
+- GET:    image/filename.ext?w=weight&h=heigth : request a image and scale (authenticated)
+- GET:    image/filename.ext?w=weight : request a image and scale (authenticated)
+- POST:   image: post a new image, body is a multipart with file and (optional) filename (to overwrite the original) (authenticated)
+- DELETE: image/filename.ext : delete a image (authenticated)
+- GET:    file/filename.ext : request a file
+- POST:   file: post a new file, body is a multipart with file and (optional) filename (to overwrite the original) (authenticated)
+- DELETE: file/filename.ext : delete a file (authenticated)
+
+## Tuning
+
+Some fine tuning can be done modifing the code:
+- Image size: edit images/index.js maxBytes parameter
+- File size: edit files/index.js maxBytes parameter
+- Maximum weigth and heigth accepted: edit MAX_SCALE in images/get.js
+- Image type validation: edit validateContentType function in images/post.js
+- File type validation: edit validateContentType function in files/post.js
+
+## Libraries
+
+The server is done thanks to existing libraries / tools, the most important are:
+- [hapi](https://hapijs.com/) rest
+- [sharp](https://github.com/lovell/sharp) image processing
+- [azure-storage](https://github.com/Azure/azure-storage-node) storage
+- [asynquence](https://github.com/getify/asynquence) control flow
+
+## Contributions
+
+Feel free to fork, update and pr. I'll be happy to discuss further improvements.
+Cheers.
+
 
--- images/get.js
+++ images/get.js
@@ -8,14 +8,15 @@ const fileutil = require('../lib/util');
 const blobSvc = azure.createBlobService();
 const IMG_CONTAINER_NAME = process.env.IMG_CONTAINER_NAME || 'images';
 const TMP_IMAGE_FOLDER = './temp_img/';
+const MAX_SCALE = 4000;
 
-module.exports = function (options) {
-  var opt = options;
-  console.log('images get', 'init');
-  fileutil.clearTempFolder(TMP_IMAGE_FOLDER); // called once on init
+module.exports = function(options) {
+var opt = options;
+console.log('images get', 'init');
+fileutil.clearTempFolder(TMP_IMAGE_FOLDER); // called once on init
 
 
-  return function (request, reply) {
+return function(request, reply) {
 
     console.log('Content mgr', 'Images', 'Get');
     let filename = request.params.blobname;
@@ -23,84 +24,84 @@ module.exports = function (options) {
     let cachename = [TMP_IMAGE_FOLDER, request.query.w || 'nW', 'x', request.query.h || 'nH', filename].join('');
     if (filename) {
 
-      try {
-        // try get the scaled version
-        fileutil.accessSync(cachename);
-        console.log('Content mgr', 'Images', 'Get Image from cache');
-        reply.file(cachename);
-
-      } catch (fileNotCachedError) {
-
-        ASQ((done) => {
-
-          // fetch from blob
-          console.log('Content mgr', 'Images', 'Get Blob', filename);
-
-          console.time(tempname);
-          blobSvc.getBlobToLocalFile(
-            IMG_CONTAINER_NAME,
-            filename,
-            tempname,
-            done.errfcb);
-
-        }).then((done, res, res2) => {
-
-          // resize if needed
-          console.timeEnd(tempname);
-          if (res2.isSuccessful) {
-            resize(tempname, request.query, request.auth.isAuthenticated, cachename, done.errfcb);
-          } else {
-            done();
-          }
-
-        }).then((done, result) => {
-
-          // reply the img
-          if (result) {
+        try {
+            // try get the scaled version
+            fileutil.accessSync(cachename);
+            console.log('Content mgr', 'Images', 'Get Image from cache');
             reply.file(cachename);
-          } else {
-            reply.file(tempname);
-          }
 
-          done();
+        } catch ( fileNotCachedError ) {
 
-          // clear the temp image
-          setTimeout(fileutil.clearTempFile(tempname), process.env.UNLINK_TIMEOUT || 2000);
+            ASQ((done) => {
 
-        }).or((err) => {
+                // fetch from blob
+                console.log('Content mgr', 'Images', 'Get Blob', filename);
 
-          // manage errors
-          if (err.name === 'StorageError' && err.message === 'NotFound') {
-            reply(Boom.notFound());
-          } else {
-            console.error(err);
-            reply(err);
-            throw err;
-          }
+                console.time(tempname);
+                blobSvc.getBlobToLocalFile(
+                    IMG_CONTAINER_NAME,
+                    filename,
+                    tempname,
+                    done.errfcb);
 
-        });
-      }
+            }).then((done, res, res2) => {
+
+                // resize if needed
+                console.timeEnd(tempname);
+                if (res2.isSuccessful) {
+                    resize(tempname, request.query, request.auth.isAuthenticated, cachename, done.errfcb);
+                } else {
+                    done();
+                }
+
+            }).then((done, result) => {
+
+                // reply the img
+                if (result) {
+                    reply.file(cachename);
+                } else {
+                    reply.file(tempname);
+                }
+
+                done();
+
+                // clear the temp image
+                setTimeout(fileutil.clearTempFile(tempname), process.env.UNLINK_TIMEOUT || 2000);
+
+            }).or((err) => {
+
+                // manage errors
+                if (err.name === 'StorageError' && err.message === 'NotFound') {
+                    reply(Boom.notFound());
+                } else {
+                    console.error(err);
+                    reply(err);
+                    throw err;
+                }
+
+            });
+        }
 
     } else {
-      reply(Boom.badRequest());
+        reply(Boom.badRequest());
     }
-  };
+};
 };
 
 
 /** resize image if width is set */
 function resize(filename, args, auth, savename, callback) {
-  try {
-    if (auth && args.w && args.h && args.w<4000 && args.h<4000) {
-      return require('sharp')(filename).resize(args.w * 1, args.h * 1).toFile(savename, callback);
-    } else if (auth && args.w && args.w<4000 && (!args.h)) {
-      return require('sharp')(filename).resize(args.w * 1).toFile(savename, callback);
-    } else {
-      return callback();
+    try {
+        if (auth && args.w && args.h && args.w < MAX_SCALE && args.h < MAX_SCALE) {
+            return require('sharp')(filename).resize(args.w * 1, args.h * 1).toFile(savename, callback);
+        } else if (auth && args.w && args.w < MAX_SCALE && (!args.h)) {
+            return require('sharp')(filename).resize(args.w * 1).toFile(savename, callback);
+        } else {
+            return callback();
+        }
+    } catch ( exception ) {
+        console.log('Sharp not available for resizing', exception);
+        return callback();
     }
-  }catch(exception){
-    console.log('Sharp not available for resizing',exception);
-    return callback();
-  }
 }
 
